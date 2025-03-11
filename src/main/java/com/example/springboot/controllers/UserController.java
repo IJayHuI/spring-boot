0bd0884b39c842b7f46f76a2775f3d26d13fbbd7
IJayHuI <2949763264@qq.com>
2025-03-11 11:59:10 +0800

    test

--- src/main/java/com/example/springboot/controllers/UserController.java
+++ src/main/java/com/example/springboot/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.example.springboot.services.IUserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+// Test: New UserController
 @RestController
 @RequestMapping("/user")
 public class UserController {
