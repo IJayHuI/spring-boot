54bcf8dc170cab481b22655f00a39717d834d6d8
IJayHuI <2949763264@qq.com>
2025-03-31 20:00:01 +0800

    Revert "重开吧"
    This reverts commit 2f0bb95c

---
+++ src/main/java/com/example/springboot/services/UserServices.java
@@ -0,0 +1,12 @@
+package com.example.springboot.services;
+
+import com.example.springboot.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServices implements IUserServices {
+    @Override
+    public void add(UserDto user) {
+
+    }
+}
